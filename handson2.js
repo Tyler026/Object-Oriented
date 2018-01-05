a24aeb7b57fa62f67dba8d87265c90fde7528359
Tyler Fuqua <tyler@agilityda.com>
2018-01-04 18:34:36 -0700

    Last changes

--- handson2.js
+++ handson2.js
@@ -1,21 +1,42 @@
 class Employee {
   constructor(name, salary, hireDate) {
-    this._name = name;
-    this._salary = salary;
-    this._hireDate = hireDate;
+    this.name = name;
+    this.salary = salary;
+    this.hireDate = hireDate;
   }
-  getName() {
+  get Name() {
     console.log(this.name.toUpperCase());
-  }
-  set Name(value) {
-
-  }
-  getSalary() {
+   }
+  get Salary() {
     console.log(this.salary);
   }
-  getHireDate() {
+  get HireDate() {
     console.log(this.hireDate);
   }
 }
+class Manager extends Employee {
+  constructor(job, years, degree, name, salary, hireDate) {
+    super(name, salary, hireDate);
+    this.job = job;
+    this.years = years;
+    this.degree = degree;
+  }
+  jobDescription() {
+    console.log(this.job);
+  }
+  yearsExperience() {
+    console.log(this.years);
+  }
+  degreeComleted() {
+    console.log(this.degree);
+  }
+}
 
-
+let Employee1 = new Manager ("Development", 2018, "Masters");
+Employee1.jobDescription();
+Employee1.yearsExperience();
+Employee1.degreeComleted();
+let newEmployee = new Employee ("tyler", 1500, "January 5th")
+newEmployee.Name;
+newEmployee.Salary;
+newEmployee.HireDate;
